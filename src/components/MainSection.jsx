2a6c254ca39c92b03fd32f26f4c312bc9b8ca729
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-10-18 15:40:51 +0100

    Update MainSection.jsx

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -10,7 +10,7 @@ import {
   DropdownButton,
 } from "react-bootstrap";
 
-const MainSection = ({ jobsArray }) => {
+const MainSection = ({ jobsArray, searchQuery }) => {
   //   const [searchQuery, setSearchQuery] = useState("");
   //   const [skip, setSkip] = useState(0);
   //   const [jobsArray, setJobsArray] = useState([]);
@@ -20,34 +20,40 @@ const MainSection = ({ jobsArray }) => {
     <div>
       {/* {console.log(jobsArray.data)} */}
       <Container>
-        <h2>Jobs</h2>
-        <Row className="border">
-          {/* <Row border="primary"> */}
-          <Col xs={4}>
-            <h2>Role</h2>
-          </Col>
-          <Col xs={4}>
-            <h2>Company</h2>
-          </Col>
-          <Col xs={4}>
-            <h2>Categories</h2>
-          </Col>
-        </Row>
-        {jobsArray.map((job) => (
-          <Row key={job._id} className="border">
-            <Col xs={4} className="border pt-3">
-              <h4> {job.title}</h4>
-            </Col>
-            <Col xs={4} className="border pt-3">
-              <Link to={`/company-detail/${job.company_name}`}>
-                <h4>{job.company_name}</h4>
-              </Link>
-            </Col>
-            <Col xs={4} className="border pt-3">
-              <h4> {job.category}</h4>
-            </Col>
-          </Row>
-        ))}
+        {searchQuery === "" ? (
+          ""
+        ) : (
+          <div>
+            <h2>Jobs</h2>
+            <Row className="border">
+              {/* <Row border="primary"> */}
+              <Col xs={4}>
+                <h2>Role</h2>
+              </Col>
+              <Col xs={4}>
+                <h2>Company</h2>
+              </Col>
+              <Col xs={4}>
+                <h2>Categories</h2>
+              </Col>
+            </Row>
+            {jobsArray.map((job) => (
+              <Row key={job._id} className="border">
+                <Col xs={4} className="border pt-3">
+                  <h4> {job.title}</h4>
+                </Col>
+                <Col xs={4} className="border pt-3">
+                  <Link to={`/company-detail/${job.company_name}`}>
+                    <h4>{job.company_name}</h4>
+                  </Link>
+                </Col>
+                <Col xs={4} className="border pt-3">
+                  <h4> {job.category}</h4>
+                </Col>
+              </Row>
+            ))}
+          </div>
+        )}
       </Container>
     </div>
   );
